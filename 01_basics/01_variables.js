e0d543708fdf72de151f41b47e0c1236feec39ad
pankaj betewad <pankajbetewad@gmail.com>
2025-03-11 06:10:21 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234  //can not be changed
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+// accountId = 2313     not allowed 
+
+console.log(accountId);
+
